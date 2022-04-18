77fc222cffe4fdd5ba65fea6c0d25ceb32ea95b5
Sanmitr564 <generic39542@gmail.com>
2022-04-18 16:44:53 -0700

    Bot Basics

--- remindme_bot.py
+++ remindme_bot.py
@@ -1,2 +1,16 @@
-import time
+import datetime
 import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+CLIENT_ID = getenv("CLIENT_ID")
+CLIENT_SECRET = getenv("CLIENT_SECRET")
+
+bot = commands.Bot(command_prefix='pm! ')
+reminders = []
+
+@bot.command
+async def pingMe(ctx: commands.Context):
+    
